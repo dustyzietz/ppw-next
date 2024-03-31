03cd463c8e94760ef16b7aa3a3853838541143a2
dzietz <dustyzietz@gmail.com>
2024-03-30 16:17:21 -1000

    got page started

---
+++ app/account/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Account() {
+  return (
+    <main >
+      <div className="container max-w-5xl w-full items-center ">
+       <h1>Custom Price Point Wholesale</h1>
+        <a className="text-xl block" href="https://pricepointwholesale/shop"> All Products </a>
+        <a href="/account"> Custom Orders </a>
+        <a href="/products"> Resellers Products</a>
+      
+      </div>
+    </main>
+  );
+}
---
+++ app/products/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Products() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+       <h1>Custom Price Point Wholesale</h1>
+        <a href="https://pricepointwholesale/shop"> All Products </a>
+        <a href="/account"> Custom Orders </a>
+        <a href="/products"> Resellers Products</a>
+      
+      </div>
+    </main>
+  );
+}
